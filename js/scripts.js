0ed2fa9beb334615a0a1d55743ba737f11e86e8b
silvermistshadow <bmead72@gmail.com>
2019-06-10 13:01:11 -0700

    complete next stage of specs, output to page as well as console

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,23 @@ const dave = "I'm sorry, Dave. I'm afraid I can't do that."
 
 var beepBoop = function(inputNum){
   var digits = [];
-  var convString = [];
+  var convArray = [];
   for (var x = 0; x <= inputNum; x++){
     var i = x.toString();
-     if (i.includes("0")) {
-       convString.push(beep);
-       console.log(convString);
+    if (i.includes("1")){
+      convArray.push(boop);
+      console.log(convArray)
+    }
+     else if (i.includes("0")) {
+       convArray.push(beep);
+       console.log(convArray);
+     }
+     else {
+       convArray.push(x);
      }
   };
-
-
+  var convString = convArray.join(", ");
+  return convString;
 };
 
 
@@ -25,6 +32,13 @@ $(document).ready(function(){
     $("#outputErr").hide();
     var inputNum = $("input#input").val();
     var outputNum = beepBoop(inputNum);
+    if (outputNum != false) {
+      $("#output").text(outputNum)
+      $("#output").show();
+    }
+    else {
+      $("#outputErr").show();
+    }
   });
 
 
